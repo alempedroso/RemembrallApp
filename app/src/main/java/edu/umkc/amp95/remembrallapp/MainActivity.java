53a757689643070af92c141be1b0536b070ce469
Alexandre <alempedroso@gmail.com>
2015-03-19 18:42:43 -0500

    Initial Commit

--- app/src/main/java/edu/umkc/amp95/remembrallapp/MainActivity.java
+++ app/src/main/java/edu/umkc/amp95/remembrallapp/MainActivity.java
@@ -80,6 +80,7 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        //blabla
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
